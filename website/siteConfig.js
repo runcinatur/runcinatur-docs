3aac6b639c339736c39fb0f10c1f2d492660f46d
jsantos.class <jsantos.class@gmail.com>
2019-10-02 23:26:33 -0400

    Atualização da documentação e publicação da BETA

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: 'Runcinatur', // Title for your website.
   tagline: 'Build plugin wordpress',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  url: 'https://runcinatur.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
